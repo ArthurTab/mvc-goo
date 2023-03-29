e29609fa661f3e89bd0439fb9144828c427d923b
Arthur Salomon <arthursalomon4@gmail.com>
2023-03-29 12:40:49 +0200

    Ajout de la premiere version

---
+++ mvc-goo-01/include/configuration.php
@@ -0,0 +1,4 @@
+<?php
+//On va recup tous les fichiers de "include"
+require_once 'parametre.php';
+require_once 'modele.php';
---
+++ mvc-goo-01/mod_accueil/accueil.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Routeur de la classe Accueil
+ */
+
+class accueil{
+
+}
